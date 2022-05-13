0eeef9c39280ac513fe434722cae5346d65a1992
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 10:35:34 +0530

    UC1-CreateDatabase

---
+++ EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO_employee_Payroll
+{
+    internal class EmployeeRepository
+    {
+        //Give path for Database Connection
+        public static string connection = @"Server=LAPTOP-ABMNAC9K\SQLEXPRESS;Database=payroll;Trusted_Connection=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_employee_Payroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Payroll Services using ADO!");
+        }
+    }
+}
